df1d4b78510865950b8f58e0000cf34659fa881d
Yuqi Zhang <yanwuzhidie@sina.com>
2016-07-22 23:19:44 +0100

    Creation

---
+++ src/CSV/ControlledStop.java
@@ -0,0 +1,35 @@
+package CSV;
+
+import CSV.ControlledStop;
+
+final class ControlledStop implements Runnable { 
+	private volatile boolean done = false;
+
+	@Override public void run() { 
+		while (!done) {
+		try {
+			// ...
+			Thread.currentThread().sleep(1000); 
+			// Do something 
+			shutdown();
+
+			System.out.println(Thread.currentThread().getName());
+
+			} catch(InterruptedException ie) {
+			Thread.currentThread().interrupt(); // Reset interrupted status 
+			} 
+		}
+	}
+	public void shutdown() { 
+		done = true;
+		}
+    public static void main(String[] args) throws Exception { 
+		
+		for (int i = 0; i < 3; i++) {
+			ControlledStop cnTest = new ControlledStop();
+			Thread t = new Thread (cnTest);
+			t.start();
+		}
+	}
+
+	}
